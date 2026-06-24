0da57c87b52eea0ff2ae858fc3b53e9ab15c93f8
vaibhavn02 <vaibhavn02@gmail.com>
2026-06-24 21:14:39 +0530

    Delete productRoutes.js

--- productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getProducts } = require('../controllers/productControllers');
-
-router.get('/', getProducts);
-
-module.exports = router;
